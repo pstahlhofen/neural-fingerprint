e002a45104fa7272180d0f02d44b7c345f54c42a
Paul Stahlhofen <paul.stahlhofen@uni-bielefeld.de>
2021-07-19 21:08:47 +0200

    Created a table of parameter-files and output-files to be used in the job-array Created a new sbatch script and a top-level calling script which reads in the table

---
+++ experiment_scripts/run_hyperparameter_search.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+#SBATCH --time 10:00:00
+#SBATCH --cpus-per-task 20
+#SBATCH --array 1-4691%5
+
+parout=($(echo ${!SLURM_ARRAY_TASK_ID} | sed 's/\([^[:space:]]*\)[[:space:]]*\([^[:space:]]*\).*/\1\n\2/'))
+par=${parout[0]}
+out=${parout[1]}
+./eval_methods.py < $par > $out
